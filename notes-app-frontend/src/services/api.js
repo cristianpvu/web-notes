1cc9ff37574deef80075a34f488b734ff5673ea4
cristi <parvucristian23@stud.ase.ro>
2026-01-07 21:13:50 +0200

    axios interceptor for CRUD operations

--- notes-app-frontend/src/services/api.js
+++ notes-app-frontend/src/services/api.js
@@ -11,6 +11,15 @@ const api = axios.create({
 })
 
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+
 export const sendMagicLink = async (email) => {
   const response = await api.post('/auth/login', { email })
   return response.data
@@ -22,4 +31,32 @@ export const verifyToken = async (accessToken) => {
   return response.data
 }
 
+export const getNotes = async (filters = {}) => {
+  const response = await api.get('/notes', { params: filters })
+  return response.data
+}
+
+
+export const getNoteById = async (id) => {
+  const response = await api.get(`/notes/${id}`)
+  return response.data
+}
+
+
+export const createNote = async (noteData) => {
+  const response = await api.post('/notes', noteData)
+  return response.data
+}
+
+
+export const updateNote = async (id, noteData) => {
+  const response = await api.put(`/notes/${id}`, noteData)
+  return response.data
+}
+
+
+export const deleteNote = async (id) => {
+  const response = await api.delete(`/notes/${id}`)
+  return response.data
+}
 export default api
